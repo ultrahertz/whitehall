06225c41e1babae2145a2b3125041d982a4e1ccb
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-05-11 10:15:15 +0100

    Use edition when archiving worldwide priorities
    This migration incorrectly used the latest edition when archiving
    worldwide priorities which are actual editions. This commit modifies the
    data migration to use the published edition directly rather than the
    latest_edition.

--- db/data_migration/20150427095111_archive_all_worldwide_priorities.rb
+++ db/data_migration/20150427095111_archive_all_worldwide_priorities.rb
@@ -1,7 +1,6 @@
 reason = "This information has been archived. See [what the UK government is doing around the world](https://www.gov.uk/government/world)."
 
-WorldwidePriority.where(state: "published").each do |wp|
-  edition = wp.latest_edition
+WorldwidePriority.where(state: "published").each do |edition|
   puts "Archiving #{edition.title} - edition #{edition.id}"
   edition.build_unpublishing(explanation: reason, unpublishing_reason_id: UnpublishingReason::Archived.id)
   archiver = Whitehall.edition_services.archiver(edition)
