13373609bbbe8ff3b143b7ee903856b773fa8e2e
Jacob Ashdown <jcbashdown@gmail.com>
2015-06-02 12:28:34 +0100

    Begin things to migrate british-office-taipei slug
    Currently on hold as we aren't sure if this is correct for world
    organisations

---
+++ db/data_migration/20150602112430_british_office_taipei_slug_changes.rb
@@ -0,0 +1,10 @@
+#commented out as we aren't sure currently if this is correct for world organisations
+
+#require 'lib/data_hygiene/change_organisation_slug'
+
+#old_slug = 'british-trade-cultural-office-taiwan'
+#new_slug = 'british-office-taipei'
+
+#binding.pry
+
+#change_slug(old_slug, new_slug)
